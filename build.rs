1612b2b9838e7d997dfd2b82b0a4139bded361e1
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-01-18 17:58:05 +0900

    New version, remove submodule etc, fixed bitfields for now...

---
+++ build.rs
@@ -0,0 +1,23 @@
+extern crate bindgen;
+
+fn main() {
+    // // Tell cargo to tell rustc to link the system bzip2
+    // // shared library.
+    // println!("cargo:rustc-link-lib=bz2");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=../chainblocks/include/chainblocks.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("../chainblocks/include/chainblocks.h")
+        .clang_arg("-I../chainblocks/deps/stb")
+        .clang_arg("-DNO_BITFIELDS")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file("src/chainblocksc.rs")
+        .expect("Couldn't write bindings!");
+
+    println!("Done processing chainblocks.h");
+}
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod chainblocksc;
+
+#[cfg(test)]
+mod tests {
+    use crate::chainblocksc::CBVar;
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+        let var: CBVar;
+    }
+}
