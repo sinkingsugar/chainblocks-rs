4b72ca3236a10a5424425a1e08f84bdd60d2f9c8
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-01-19 17:36:13 +0900

    more messing around, rust is too strict for me but it's not bad

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,97 @@ use crate::chainblocksc::CBVar;
 use crate::chainblocksc::CBTypesInfo;
 use crate::chainblocksc::CBContext;
 use crate::chainblocksc::CBlock;
+use crate::chainblocksc::CBCore;
+use crate::chainblocksc::CBSeq;
+use crate::chainblocksc::chainblocksInterface;
 use std::ffi::CString;
+use std::convert::TryInto;
+
+const ABI_VERSION: u32 = 0x20200101;
+
+extern crate dlopen;
+use dlopen::symbor::Library;
+
+fn try_load_dlls() -> Option<Library> {
+    return None;
+}
+
+#[ctor]
+static Core: CBCore = {
+    let lib = Library::open_self()
+        .ok()
+        .or_else(try_load_dlls)
+        .unwrap();
+    
+    let fun =
+        lib.symbol::<unsafe extern "C" fn(abi_version: u32)->CBCore>("chainblocksInterface")
+        .unwrap();
+
+    let core = fun(ABI_VERSION);
+    if core.registerBlock.is_none() {
+        panic!("Failed to aquire chainblocks interface, version not compatible.");
+    }
+    core
+};
+
+#[inline(always)]
+fn length(seq: CBSeq) -> u64 {
+    unsafe {
+        let arr = seq as *mut std::ffi::c_void;
+        return Core
+            .arrayLength
+            .unwrap()
+            (arr);
+    }
+}
+
+#[inline(always)]
+fn free(seq: CBSeq) {
+    unsafe {
+        let arr = seq as *mut std::ffi::c_void;
+        Core
+            .arrayFree
+            .unwrap()
+            (arr);
+    }
+}
+
+struct Seq {
+    cseq: CBSeq
+}
+
+struct SeqIterator {
+    seq: Seq,
+    count: u64
+}
+
+impl Iterator for SeqIterator {
+    type Item = CBVar;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < length(self.seq.cseq) {
+            unsafe {
+                let item = *self.seq.cseq.offset(self.count.try_into().unwrap());
+                self.count += 1;
+                Some(item)
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl IntoIterator for Seq {
+    type Item = CBVar;
+    type IntoIter = SeqIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        return SeqIterator{
+            seq: self,
+            count: 0
+        };
+    }
+}
 
 trait Var {
 }
@@ -29,7 +119,7 @@ trait Block {
     // fn inputTypes(&self) -> CBTypesInfo;
     // fn outputTypes(&self) -> CBTypesInfo;
 
-    fn setParam(&self, index: i32, value: CBVar) {}
+    fn setParam(&self, index: i32, value: &CBVar) {}
     fn getParam(&self, index: i32) {}
 
     fn activate(&self, context: &CBContext, input: &CBVar) -> CBVar;
@@ -69,76 +159,45 @@ unsafe extern "C" fn cblock_help<T: Default + Block>(arg1: *mut CBlock) -> *cons
             .as_ptr();
 }
 
+unsafe extern "C" fn cblock_setup<T: Default + Block>(arg1: *mut CBlock) {
+    let blk = arg1 as *mut BlockWrapper<T>;
+    (*blk).block.setup();
+}
+
+unsafe extern "C" fn cblock_destroy<T: Default + Block>(arg1: *mut CBlock) {
+    let blk = arg1 as *mut BlockWrapper<T>;
+    (*blk).block.destroy();
+}
+
+unsafe extern "C" fn cblock_activate<T: Default + Block>(arg1: *mut CBlock,
+                                                         arg2: *mut CBContext,
+                                                         arg3: *const CBVar) -> CBVar {
+    let blk = arg1 as *mut BlockWrapper<T>;
+    let ctx = arg2.as_ref();
+    let value = arg3.as_ref();
+    return (*blk).block.activate(ctx.unwrap(), value.unwrap());
+}
+
+unsafe extern "C" fn cblock_cleanup<T: Default + Block>(arg1: *mut CBlock) {
+    let blk = arg1 as *mut BlockWrapper<T>;
+    (*blk).block.cleanup();
+}
+
 fn create<T: Default + Block>() -> BlockWrapper<T> {
     return BlockWrapper::<T>{
         header: CBlock{
             name: Some(cblock_name::<T>),
             help: Some(cblock_help::<T>),
+            setup: Some(cblock_setup::<T>),
+            destroy: Some(cblock_destroy::<T>),
+            activate: Some(cblock_activate::<T>),
+            cleanup: Some(cblock_cleanup::<T>),
             ..Default::default()
         },
         block: T::default(),
         name: None,
         help: None
     };
-}
-
-#[cfg(dllblock)]
-mod dllblock {
-    use crate::chainblocksc::CBVar;
-    use crate::chainblocksc::CBCore;
-    use crate::chainblocksc::chainblocksInterface;
-    
-    extern crate dlopen;
-    use dlopen::symbor::Library;
-
-    static mut Core: CBCore = CBCore{
-        registerBlock: None,
-        registerObjectType: None,
-        registerEnumType: None,
-        registerRunLoopCallback: None,
-        unregisterRunLoopCallback: None,
-        registerExitCallback: None,
-        unregisterExitCallback: None,
-        findVariable: None,
-        throwException: None,
-        suspend: None,
-        cloneVar: None,
-        destroyVar: None,
-        freeArray: None,
-        validateChain: None,
-        runChain: None,
-        validateBlocks: None,
-        runBlocks: None,
-        log: None,
-        createBlock: None,
-        createChain: None,
-        destroyChain: None,
-        createNode: None,
-        destroyNode: None,
-        schedule: None,
-        tick: None,
-        sleep: None
-    };
-
-    fn try_load_dlls() -> Option<Library> {
-        return None;
-    }
-
-    #[ctor]
-    fn attach() {
-        let lib = Library::open_self()
-            .ok()
-            .or_else(try_load_dlls)
-            .unwrap();
-        
-        let fun = unsafe{
-            lib.symbol::<unsafe extern "C" fn()->CBCore>("chainblocksInterface")
-        }.unwrap();
-
-        unsafe {
-            Core = fun();
-        }
-    }
 }
 
 #[cfg(test)]
@@ -157,9 +216,7 @@ mod dummy_block {
 
     impl Block for DummyBlock {
         fn name(&self) -> String { "Dummy".to_string() }
-        fn activate(&self, context: &CBContext, input: &CBVar) -> CBVar {
-            return CBVar{ ..Default::default() };
-        }
+        fn activate(&self, context: &CBContext, input: &CBVar) -> CBVar { CBVar::default(); }
     }
 
     #[test]
